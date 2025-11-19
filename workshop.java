f88aea63231e9b7b38e97bce1db1064d99a2a805
Aalish <np01cp4a250021@islingtoncollege.edu.np>
2025-11-19 09:59:13 +0545

    Initial sharing of project

---
+++ workshop.java
@@ -0,0 +1,13 @@
+public class workshop
+{
+    int age=15; //instance variable
+    static int age_1=51; //static variable
+    
+    public static void main(String[] args)
+    {
+        int age=123; //local variable 
+        workshop a = new workshop();
+        System.out.println(a.age);
+        System.out.println(workshop.age_1);
+    }
+}
